4dac4f6c8679ba4f763d311a26ce0f92b5789e01
jribeiro79 <jribeiro79@gmail.com>
2024-10-31 15:25:17 +0000

    Final version after draft development

--- soccer-app/src/api.js
+++ soccer-app/src/api.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-// const API_URL = 'https://localhost:44314/api';
-// const API_URL = 'https://soccerapi20241029180755.azurewebsites.net/api';
 const API_URL = process.env.REACT_APP_API_URL;
 
 export const fetchTeams = () => axios.get(`${API_URL}/teams`);
